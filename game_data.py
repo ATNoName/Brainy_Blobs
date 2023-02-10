79e4ed71a5f437f38a30a5c14a5112b0cc760a57
ATNoName <65098676+ATNoName@users.noreply.github.com>
2023-02-10 18:04:24 -0500

    Update 5: Finish Board Implementation

--- game_data.py
+++ game_data.py
@@ -15,17 +15,14 @@ class Board:
 
     def process_movement(self, blob_location_list=[], blob_number_list=[], target_list=[]):
         # process blob movement
-        action_processed = [False] * len(target_list)
-
+        action_processed = [False]*len(target_list)
+        # Remove all blobs that are moving from space
+        for i in range(len(target_list)):
+            start_space = self.board_state[blob_location_list[i][0]][blob_location_list[i][1]]
+            start_space.add_number(-blob_number_list[i])
+        # Process all movement collisions
         for i in range(len(target_list)):
             if not action_processed[i]:
-                # start the movement process
-                action_processed[i] = True
-                target_space = self.board_state[target_list[i][0]][target_list[i][1]]
-                start_space = self.board_state[blob_location_list[i][0]][blob_location_list[i][1]]
-                owner = start_space.get_owner()
-                start_space.add_number(-blob_number_list[i])
-
                 # check if movementcollision needs to be processed
                 if target_list[i] in blob_location_list:
                     target_index = blob_location_list.index(target_list[i])
@@ -34,19 +31,46 @@ class Board:
                         if not result[0]:
                             blob_number_list[i] = 0
                             blob_number_list[target_index] = result[1]
-                            break
+                            action_processed[i] = True
                         else:
                             blob_number_list[i] = result[1]
-
-                # check if areacollision needs to be processed
+                            blob_number_list[target_index] = 0
+                            action_processed[target_index] = True
+        # Process area collisions
+        for i in range(len(target_list)):
+            if not action_processed[i]:
+            # check if areacollision needs to be processed
                 blob_list = [blob_number_list[i]]
+                blob_owner = [i]
+                target_space = self.board_state[target_list[i][0]][target_list[i][1]]
                 for j in range(i, len(target_list)):
                     if target_list[i] == target_list[j]:
                         blob_list.append(target_list[j])
-                # move the damn thing
-
-                # process area conquest
-
+                        blob_owner.append[j]
+                if target_space.get_number() > 0:
+                    blob_list.append(target_space.get_number())
+                    blob_owner.append[-1]
+                if len(blob_list) != 1:
+                    result = self.process_areacollision(blob_list)
+                    for player in blob_owner:
+                        if result[0] != blob_owner[player]:
+                            if blob_owner[player] >= 0:
+                                action_processed[player] = True
+                                blob_number_list[player] = 0
+                            else:
+                                target_space.set_number(0)
+                        else:
+                            blob_number_list = result[1]
+        # Move any survivors to their target square and process any area conquest
+        for i in range(len(target_list)):
+            if not action_processed[i]:
+                target_space = self.board_state[target_list[i][0]][target_list[i][1]]
+                owner = self.board_state[blob_location_list[i][0]][blob_location_list[i][1]].get_owner()
+                if (target_space.get_owner() != owner):
+                    self.conquer_space(owner, target_list[i][0], target_list[i][1])
+        # Process encirclement
+        self.process_encirclement()
+    
     def conquer_space(self, owner, x, y):
         # Note: base elimination is included in this
         if self.board_state[x][y].get_type() == 1:
@@ -66,8 +90,13 @@ class Board:
     def process_areacollision(self, blob_list=list()):
         # process blob collision
         first_max = max(blob_list)
-        second_max = max(blob_list.pop(first_max))
-        return blob_list.index(first_max), first_max - second_max
+        index = blob_list.index(first_max)
+        blob_list[index] = 0
+        second_max = max(blob_list)
+        blob_list[index] = first_max
+        if first_max == second_max:
+            return None, 0
+        return index, first_max - second_max
 
     def process_movementcollision(self, blob1, blob2):
         # this function is used for when two opposing blob
@@ -85,25 +114,57 @@ class Board:
         for blob in blob_list:
             merged_blob += blob
         return merged_blob
+    
+    def process_encirclement(self):
+        # This function is processed when a group of blob is "surrounded" by other blobs.
+        # Corners and edges require less square to occupy
+        # Process: mark all encircled squares, then process the encirclement
+        # Square with a base should be ignored
+        encircled = []
+        for x in range(self.length):
+            for y in range(self.width):
+                space = self.board_state[x][y]
+                if space.get_type() == 0:
+                    neighbouring_square = []
+                    if (not ((x + 1) == self.length)):
+                        neighbouring_square.append(self.board_state[x+1][y])
+                    if (not ((y + 1) == self.width)):
+                        neighbouring_square.append(self.board_state[x][y+1])
+                    if (not ((x -1) == -1)):
+                        neighbouring_square.append(self.board_state[x-1][y])
+                    if (not ((y -1) == -1)):
+                        neighbouring_square.append(self.board_state[x][y-1])
+                    # Condition:
+                    # 1) check if neighbouring_square belong to same owner
+                    # 2) check if neighbouring square oppose center owner
+                    # 3) check if neighbouring_square has at least 1 blob
+                    atkowner = neighbouring_square[0].get_owner()
+                    if atkowner != space.get_owner():
+                        is_encircled = True
+                        for square in neighbouring_square:
+                            if square.get_owner() != atkowner or square.get_number() == 0:
+                                is_encircled = False
+                        if is_encircled:
+                            encircled.append((space, atkowner))
+        # Eliminate the encirclement
+        for square in encircled:
+            square[0].set_number(0)
+            square[0].set_owner(square[1])
 
-    def process_encirclement(self, point=(0, 0), atkowner=nn.Player()):
-        # This function is processed when at least 4 blobs surrounds
-        # one blob
-        x, y = point
-        self.board_state[x][y].set_number(0)
-        self.board_state[x][y].set_owner(atkowner)
-        self.board_state[x - 1][y].add_number(-1)
-        self.board_state[x][y - 1].add_number(-1)
-        self.board_state[x + 1][y].add_number(-1)
-        self.board_state[x][y + 1].add_number(-1)
 
-    def generate_base(self, value):
-        # create new bases (very primitive, replace if code is better)
+    def generate_base(self, value, dist):
+        # create new bases that are far away enough from others
         for self in range(value):
-            x = random.randint(0, self.length - 1)
-            y = random.randint(0, self.width - 1)
-            player = player.Player(len(self.player_list),
-                                   (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), x, y)
+            is_suitable = False
+            while (not is_suitable):
+                is_suitable = True
+                x = random.randint(0, self.length - 1)
+                y = random.randint(0, self.width - 1)
+                for player in self.player_list:
+                    baseX, baseY = player.get_base()
+                    if abs(baseX - x) + abs(baseY - y) < dist:
+                        is_suitable = False
+            player = player.Player(len(self.player_list), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), x, y)
             self.board_state[x][y].set_type(1)
             self.board_state[x][y].set_number(10)
             self.board_state[x][y].set_owner(player)
