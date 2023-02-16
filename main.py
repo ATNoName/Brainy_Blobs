529820279e0b1f3bd1daf402999388b4e710020e
ATNoName <alan.teng2003@gmail.com>
2023-02-15 20:31:42 -0500

    Update 11: Implemented DCP and main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import game_data as gd
 # This is for running the function
 def main():
     population = 1000
@@ -7,6 +8,8 @@ def main():
     turn1 = frame_rate * 600 # how many turns for learning phase
     turn2 = frame_rate * 300 # how many turns for fighting phase
     turn3 = frame_rate * 300 # how many turns before the sim ends
+    board = gd.Board()
+    board.print_output()
 
 def test():
     """
